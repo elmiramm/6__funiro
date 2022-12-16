c339a35496fbda07f85ab130252e2606061cb0a7
elmiramm <elmira.m.m@yandex.ru>
2022-12-16 14:34:35 +0300

    gulp-html,file-include

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,18 @@ global.app = {
 //Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
 
 //Наблюдатель за изменениями в файлах
 function watcher() {
 	gulp.watch(path.watch.files, copy);
+	gulp.watch(path.watch.html, html);
 }
 
+const mainTasks = gulp.parallel(copy, html);
+
 //Построение сценария выполнения задач
-const dev = gulp.series(reset, copy, watcher);
+const dev = gulp.series(reset, mainTasks, watcher);
 
 //Выполнение сценария по умолчанию
 gulp.task('default', dev);
