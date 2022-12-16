63b6ee1c5001e49305d32c2e6359ee0eb53d8130
elmiramm <elmira.m.m@yandex.ru>
2022-12-16 18:38:02 +0300

    gulp - scss

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,5 +1,11 @@
 import replace from "gulp-replace"; //Поиск и замена
+import plumber from "gulp-plumber"; //Обработка ошибок 
+import notify from "gulp-notify"; // Сообщения при ошибке
+import browsersync from "browser-sync"; // Локальный сервер
 
 export const plugins = {
 	replace: replace,
+	plumber: plumber,
+	notify: notify,
+	browsersync: browsersync,
 }
--- gulpfile.js
+++ gulpfile.js
@@ -16,17 +16,20 @@ global.app = {
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
 
 //Наблюдатель за изменениями в файлах
 function watcher() {
 	gulp.watch(path.watch.files, copy);
 	gulp.watch(path.watch.html, html);
+	gulp.watch(path.watch.scss, scss);
 }
 
-const mainTasks = gulp.parallel(copy, html);
+const mainTasks = gulp.parallel(copy, html, scss);
 
 //Построение сценария выполнения задач
-const dev = gulp.series(reset, mainTasks, watcher);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 //Выполнение сценария по умолчанию
 gulp.task('default', dev);
