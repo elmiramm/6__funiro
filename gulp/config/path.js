0d4c5f2b585c0a9a127300141e699178eaad8381
elmiramm <elmira.m.m@yandex.ru>
2022-12-19 12:54:41 +0300

    gulp-images

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -2,19 +2,22 @@
 import * as nodePath from 'path';
 const rootFolder = nodePath.basename(nodePath.resolve());
 
-
 const buildFolder = `./dist`;
 const srcFolder = `./src`;
 
 export const path = {
 	build: {
-		js: `${buildFolder}/js`,
+		js: `${buildFolder}/js/`,
 		css: `${buildFolder}/css/`,
 		html: `${buildFolder}/`,
+		images: `${buildFolder}/img/`,
+		fonts: `${buildFolder}/fonts/`,
 		files: `${buildFolder}/files/`
 	},
 	src: {
 		js: `${srcFolder}/js/app.js`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+		svg: `${srcFolder}/img/**/*.svg`,
 		scss: `${srcFolder}/scss/style.scss`,
 		html: `${srcFolder}/*.html`,
 		files: `${srcFolder}/files/**/*.*`,
@@ -23,6 +26,7 @@ export const path = {
 		js: `${srcFolder}/js/**/*.scss`,
 		scss: `${srcFolder}/scss/**/*.scss`,
 		html: `${srcFolder}/**/*.html`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
 		files: `${srcFolder}/files/**/*.*`,
 	},
 	clean: buildFolder,
--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -2,10 +2,12 @@ import replace from "gulp-replace"; //Поиск и замена
 import plumber from "gulp-plumber"; //Обработка ошибок 
 import notify from "gulp-notify"; // Сообщения при ошибке
 import browsersync from "browser-sync"; // Локальный сервер
+import newer from "gulp-newer" //Проверка обновлений 
 
 export const plugins = {
 	replace: replace,
 	plumber: plumber,
 	notify: notify,
 	browsersync: browsersync,
+	newer: newer,
 }
